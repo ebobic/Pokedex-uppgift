2a4a8a0b17163063464bde8c12932630eff7ee48
ebobic <ebobic95@gmail.com>
2025-09-01 14:41:14 +0200

    Added RandomButton comp

---
+++ src/components/randombutton.tsx
@@ -0,0 +1,43 @@
+"use client"  // tells Next.js that this is a "client component" and means this code runs in the browser (and can use hooks like useState).
+
+import Image from "next/image";
+import getPokemonById from "@/lib/data/pokemon";
+import PokeCard from "./pokecard";
+import { useState } from "react";
+import type { Pokemon } from "@/lib/interfaces/pokemon";
+
+
+export default function RandomButton() {
+    // State variable to store the current random Pokémon.
+    // - "randomPokemon" holds the current Pokémon (or null if none yet).
+    // - "setRandomPokemon" is the function to update it.
+    const [randomPokemon, setRandomPokemon] = useState<Pokemon | null>(null);
+
+    // Function that fetches one random Pokémon and saves it to state
+    async function showRandomPokemon() {
+        const randomId = Math.floor(Math.random() * 1025) + 1;  // Generate random ID from 1-1025!
+        const pokemon = await getPokemonById(randomId);  // Fetch Pokemon data from API
+        setRandomPokemon(pokemon);  // Store the Pokemon in state
+    }
+
+    return (
+        <>
+            {/* Button that fetches a random Pokémon when clicked */}
+            <button onClick={showRandomPokemon} className="btn-primary">
+            <Image
+                src="/Dice.svg"
+                width={25}
+                height={25}
+                alt="Roll the dice!"
+            />
+            Random Pokémon</button>
+
+            {/* If "randomPokemon" exists (not null), show the Pokémon card */}
+            {randomPokemon && (
+                <section className="flex justify-center">
+                <PokeCard pokemon={randomPokemon} />
+                </section>
+            )}
+        </>
+    );
+}
--- src/lib/data/randompokemons.ts
+++ src/lib/data/randompokemons.ts
@@ -1,14 +1,15 @@
+// Random pokemon with it 1-1025!)
 export function getRandomPokemonIds(count: number): number[] {
-    const pokemonIds: number[] = [];
-    while (pokemonIds.length < count) {
-        const randomId = Math.floor(Math.random() * 1025) + 1; // visa pokemons med ID 1-1025!
-        if (!pokemonIds.includes(randomId)) {
-            pokemonIds.push(randomId);
+    const ids: number[] = [];
+    const usedIds = new Set<number>();
+    
+    while (ids.length < count) {
+        const randomId = Math.floor(Math.random() * 1025) + 1;
+        if (!usedIds.has(randomId)) {
+            ids.push(randomId);
+            usedIds.add(randomId);
         }
     }
-    return pokemonIds;
-}
-
-export function getRandomPokemonId(): number {
-    return Math.floor(Math.random() * 1025) + 1;
+    
+    return ids;
 }
