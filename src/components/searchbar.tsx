393e3f18155a958dd0238ff3e708788ea48a33e4
ebobic <ebobic95@gmail.com>
2025-09-04 16:51:34 +0200

    Improved searchbar visibility and UX!

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Searchbar() {
     return (
-        <Form action="/search" className="flex items-center justify-between w-full max-w-lg shadow-lg rounded-lg p-3 bg-white">
+        <Form action="/search" className="flex items-center justify-between w-full max-w-lg shadow-2xl rounded-lg p-3 bg-white">
             {/* On submission, the input value will be appended to
                 the URL, e.g. /search?query=abc */}
             <label htmlFor="pokemon-search" className="sr-only">
@@ -13,15 +13,15 @@ export default function Searchbar() {
                 id="pokemon-search"
                 name="query" 
                 placeholder="Search for a Pokémon..." 
-                className="flex-1 outline-none text-gray-700 placeholder-gray-400 text-sm sm:text-base"
+                className="flex-1 outline-none text-black placeholder-black text-sm sm:text-base"
                 required
             />
             <button type="submit" className="bg-[hsl(360,100%,41%)] hover:bg-red-700 rounded-md p-1 sm:p-2 transition-colors">
                 <Image
                     src="/Search.svg"
                     alt="Search"
-                    width={16}
-                    height={16}
+                    width={24}
+                    height={24}
                     className="p-0.5 sm:p-1"
                 />
             </button>
